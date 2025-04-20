f70871fcffb201b2e6d6ac26fdb896c8cfd29cae
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-20 17:24:37 -0300

    create dbAddAccount

---
+++ src/data/account/add-account/db-add-account.ts
@@ -0,0 +1,9 @@
+import { AddAccount } from '../../../domain/use-cases/add-account';
+import { Hasher } from '../../protocols/hasher';
+
+export class DbAddAccount implements AddAccount {
+  constructor(private readonly hasher: Hasher) {
+    this.hasher = hasher;
+  }
+  async add(data: AddAccount.Params): Promise<void> {}
+}
