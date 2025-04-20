8dff118cd842823a8317180402342d7621974bdf
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-20 17:55:02 -0300

    test: ensure DbAddAccount calls LoadAccountByEmailRepository with correct value

--- src/data/account/add-account/db-add-account.test.ts
+++ src/data/account/add-account/db-add-account.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, test, vi } from 'vitest';
 import { Hasher } from '../../protocols/hasher';
 import { DbAddAccount } from './db-add-account';
 import { AddAccount } from '../../../domain/use-cases/add-account';
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+import { AccountModel } from '../../../domain/use-cases/models/account';
 
 const makeHasher = (): Hasher => {
   class HasherStub implements Hasher {
@@ -12,10 +14,22 @@ const makeHasher = (): Hasher => {
 
   return new HasherStub();
 };
+const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub
+    implements LoadAccountByEmailRepository
+  {
+    loadByEmail(email: string): Promise<AccountModel> {
+      return new Promise(resolve => resolve(mockAccount()));
+    }
+  }
+
+  return new LoadAccountByEmailRepositoryStub();
+};
 
 type SutTypes = {
   sut: DbAddAccount;
   hasherStub: Hasher;
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
 };
 
 const mockAddAccountParams = (): AddAccount.Params => ({
@@ -24,16 +38,24 @@ const mockAddAccountParams = (): AddAccount.Params => ({
   password: 'any_password',
 });
 
+const mockAccount = (): AccountModel => ({
+  id: 1,
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+});
+
 const makeSut = (): SutTypes => {
   const hasherStub = makeHasher();
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository();
 
-  const sut = new DbAddAccount(hasherStub);
+  const sut = new DbAddAccount(hasherStub, loadAccountByEmailRepositoryStub);
 
-  return { sut, hasherStub };
+  return { sut, hasherStub, loadAccountByEmailRepositoryStub };
 };
 
 describe('add()', () => {
-  test('ensure DbAddAccount calls hasher with correct value', async () => {
+  test('ensure DbAddAccount calls Hasher with correct value', async () => {
     const { sut, hasherStub } = makeSut();
 
     const hashSpy = vi.spyOn(hasherStub, 'hash');
@@ -42,5 +64,18 @@ describe('add()', () => {
     await sut.add(data);
 
     expect(hashSpy).toHaveBeenCalledWith(data.password);
+  });
+  test('ensure DbAddAccount calls LoadAccountByEmailRepository with correct value', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+
+    const loadByEmailSpy = vi.spyOn(
+      loadAccountByEmailRepositoryStub,
+      'loadByEmail'
+    );
+
+    const data = mockAddAccountParams();
+    await sut.add(data);
+
+    expect(loadByEmailSpy).toHaveBeenCalledWith(data.email);
   });
 });
