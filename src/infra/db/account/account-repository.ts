26233a8278649ae56b49802b55d5d2223a8aa514
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-21 13:59:44 -0300

    feat: ensure AccountRepository.loadByEmail calls prisma with correct value

--- src/infra/db/account/account-repository.ts
+++ src/infra/db/account/account-repository.ts
@@ -1,9 +1,18 @@
 import { AddAccountRepository } from '../../../data/protocols/add-account-repository';
+import { LoadAccountByEmailRepository } from '../../../data/protocols/load-account-by-email-repository';
 import { AddAccount } from '../../../domain/use-cases/add-account';
 import prisma from '../../../../prisma/db';
+import { AccountModel } from '../../../domain/use-cases/models/account';
 
-export class AccountRepository implements AddAccountRepository {
+export class AccountRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
   async add(data: AddAccount.Params): Promise<void> {
     await prisma.user.create({ data });
+  }
+  async loadByEmail(email: string): Promise<AccountModel | null> {
+    const account = await prisma.user.findUnique({ where: { email } });
+
+    return account;
   }
 }
