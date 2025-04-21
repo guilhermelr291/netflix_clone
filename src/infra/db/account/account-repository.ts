4db8db7266baa6f1e889eb3eb4d17a34f55a3cff
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-21 13:42:17 -0300

    feat: ensure AccountRepository calls prisma with correct data

--- src/infra/db/account/account-repository.ts
+++ src/infra/db/account/account-repository.ts
@@ -1,6 +1,9 @@
 import { AddAccountRepository } from '../../../data/protocols/add-account-repository';
 import { AddAccount } from '../../../domain/use-cases/add-account';
+import prisma from '../../../../prisma/db';
 
 export class AccountRepository implements AddAccountRepository {
-  async add(data: AddAccount.Params): Promise<void> {}
+  async add(data: AddAccount.Params): Promise<void> {
+    await prisma.user.create({ data });
+  }
 }
