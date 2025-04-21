92f2c6393c4f0ad93e6f7af301570dd543cc6ed7
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-21 13:42:24 -0300

    test: ensure AccountRepository calls prisma with correct data

---
+++ src/infra/db/account/account-repository.test.ts
@@ -0,0 +1,37 @@
+import { vi, describe, test, expect } from 'vitest';
+import { AccountRepository } from './account-repository';
+import { AddAccount } from '../../../domain/use-cases/add-account';
+import prisma from '../../../../prisma/db';
+
+vi.mock('../../../../prisma/db', () => ({
+  default: {
+    user: {
+      create: vi.fn(),
+    },
+  },
+}));
+
+const mockAddAccountParams = (): AddAccount.Params => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+});
+
+const makeSut = (): AccountRepository => {
+  const sut = new AccountRepository();
+
+  return sut;
+};
+
+describe('AccountRepository', () => {
+  describe('add()', () => {
+    test('should call prisma with correct data', async () => {
+      const sut = makeSut();
+
+      const data = mockAddAccountParams();
+      await sut.add(data);
+
+      expect(prisma.user.create).toHaveBeenCalledWith({ data });
+    });
+  });
+});
