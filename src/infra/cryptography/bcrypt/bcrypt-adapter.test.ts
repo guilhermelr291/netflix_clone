52dd161134fb88f3426bfacf43501d0edb73901f
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-21 14:18:32 -0300

    test: ensure BcryptAdapter.hash calls bcrypt with correct value

---
+++ src/infra/cryptography/bcrypt/bcrypt-adapter.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test, vi } from 'vitest';
+import bcrypt, { genSalt } from 'bcrypt';
+import { BcryptAdapter } from './bcrypt-adapter';
+
+vi.mock('bcrypt', () => ({
+  default: {
+    hash: vi.fn().mockResolvedValue('hashed_value'),
+    genSalt: vi.fn().mockResolvedValue('any_salt'),
+  },
+}));
+
+const SALT = 10;
+
+const makeSut = (): BcryptAdapter => {
+  const sut = new BcryptAdapter(SALT);
+  return sut;
+};
+
+describe('BcryptAdapter', () => {
+  describe('hash()', () => {
+    test('should call bcrypt.hash with correct value', async () => {
+      const sut = makeSut();
+      const value = 'any_value';
+
+      await sut.hash(value);
+
+      expect(bcrypt.hash).toHaveBeenCalledWith(value, 'any_salt');
+    });
+  });
+});
