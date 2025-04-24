2399ff9a5964c152bd37e7356a4ac2cec9264305
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-24 20:29:50 -0300

    feat: create auth routes

--- src/main/routes/auth-routes.ts
+++ src/main/routes/auth-routes.ts
@@ -1,7 +1,27 @@
 import { Router } from 'express';
+import { SignUpController } from '../../presentation/controllers/auth/signup/sign-up-controller';
+import { FieldComparerImpl } from '../../utils/validations/field-comparer';
+import { DbAddAccount } from '../../data/account/add-account/db-add-account';
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt/bcrypt-adapter';
+import { AccountRepository } from '../../infra/db/account/account-repository';
+import { EmailValidatorImpl } from '../../infra/validations/email-validator/email-validator';
+import { adaptRoute } from '../adapters/express-route-adapter';
+
+const hasher = new BcryptAdapter(10);
+const accountRepository = new AccountRepository();
+const addAccount = new DbAddAccount(
+  hasher,
+  accountRepository,
+  accountRepository
+);
+const fieldComparer = new FieldComparerImpl('password', 'passwordConfirmation');
+const emailValidator = new EmailValidatorImpl();
+const signUpController = new SignUpController(
+  addAccount,
+  fieldComparer,
+  emailValidator
+);
 
 export default (router: Router): void => {
-  router.get('/', (req, res) => {
-    res.status(200).json({ message: 'ok' });
-  });
+  router.post('/login', adaptRoute(signUpController));
 };
