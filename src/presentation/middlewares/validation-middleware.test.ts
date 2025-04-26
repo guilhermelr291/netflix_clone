1032770e56fadd4e4e4ab02c70200504d0775838
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-26 14:21:23 -0300

    test: ensure ValidateDate middleware returns 200 status and validated data on success

---
+++ src/presentation/middlewares/validation-middleware.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test, vi } from 'vitest';
+import { ValidateData } from './validation-middleware';
+import { z } from 'zod';
+import { ok } from '../helpers/http-helper';
+
+const schema = {
+  safeParseAsync: vi.fn().mockResolvedValue({
+    success: true,
+    data: 'validated data',
+  }),
+} as unknown as z.Schema;
+
+const makeSut = (): ValidateData => new ValidateData(schema);
+
+const mockValidData = () => ({
+  name: 'any_name',
+  email: 'valid_email@mail.com',
+});
+
+describe('ValidateData', () => {
+  test('Should return ok with validated data on validation success', async () => {
+    const sut = makeSut();
+
+    const result = await sut.handle({ bodyContent: mockValidData() });
+
+    expect(result).toEqual(ok('validated data'));
+  });
+});
