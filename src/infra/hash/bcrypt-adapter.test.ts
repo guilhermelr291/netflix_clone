bff30ead05ba1d8f64a890cd495af7e991872afa
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-27 14:43:51 -0300

    test: ensure JwtAdapter calls jwt.sign with correct values

---
+++ src/infra/hash/bcrypt-adapter.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test, vi } from 'vitest';
+
+import { JwtAdapter } from './jwt-adapter';
+import jwt from 'jsonwebtoken';
+
+vi.mock('jsonwebtoken', () => ({
+  default: {
+    sign: vi.fn().mockReturnValue('encrypted_value'),
+  },
+}));
+
+const SALT = 10;
+
+const makeSut = (): JwtAdapter => {
+  process.env.JWT_SECRET = 'test_secret';
+  const sut = new JwtAdapter();
+  return sut;
+};
+
+describe('JwtAdapter', () => {
+  describe('encrypt()', () => {
+    test('should call jwt.sign with correct values', async () => {
+      const sut = makeSut();
+      const value = 'any_value';
+
+      sut.encrypt(value);
+
+      expect(jwt.sign).toHaveBeenCalledWith({ value }, 'test_secret');
+    });
+  });
+});
