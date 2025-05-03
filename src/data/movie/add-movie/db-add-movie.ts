ae5421e9879249d9e8378c83df11b315dd68e8ea
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-03 17:50:22 -0300

    feat: ensure DbAddMovie calls AddMovieRepository with correct data

--- src/data/movie/add-movie/db-add-movie.ts
+++ src/data/movie/add-movie/db-add-movie.ts
@@ -1,14 +1,18 @@
 import { Movie } from '../../../domain/models/movie';
 import { AddMovie } from '../../../domain/use-cases/movie/add-movie';
 import { ConflictError } from '../../../shared/errors';
+import { AddMovieRepository } from '../../protocols/movie/add-movie-repository';
 import { LoadMovieByTitleRepository } from '../../protocols/movie/load-movie-by-title-repository';
 
 export class DbAddMovie implements AddMovie {
   constructor(
-    private readonly loadMovieByTitleRepository: LoadMovieByTitleRepository
+    private readonly loadMovieByTitleRepository: LoadMovieByTitleRepository,
+    private readonly addMovieRepository: AddMovieRepository
   ) {}
   async add(data: AddMovie.Params): Promise<Movie> {
     const movie = await this.loadMovieByTitleRepository.loadByTitle(data.title);
     if (movie) throw new ConflictError('Movie with this title already exists');
+
+    await this.addMovieRepository.add(data);
   }
 }
