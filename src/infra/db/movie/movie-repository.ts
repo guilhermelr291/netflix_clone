514d6fdb76f9545befa01796bc1ec7632e6c603d
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-03 18:37:48 -0300

    feat: ensure MovieRepository.loadByTitle calls prisma with correct value

--- src/infra/db/movie/movie-repository.ts
+++ src/infra/db/movie/movie-repository.ts
@@ -1,7 +1,9 @@
-import { AddMovieRepository } from '../../../data/protocols/movie/add-movie-repository';
+import prisma from '../../../../prisma/db';
 import { LoadMovieByTitleRepository } from '../../../data/protocols/movie/load-movie-by-title-repository';
 import { Movie } from '../../../domain/models/movie';
 
 export class MovieRepository implements LoadMovieByTitleRepository {
-  async loadByTitle(title: string): Promise<Movie | null> {}
+  async loadByTitle(title: string): Promise<Movie | null> {
+    const movie = await prisma.movie.findUnique({ where: { title } });
+  }
 }
