408d69899ea6620623b403f36094a6f5f372b0f0
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-05 18:22:59 -0300

    ensure DbLoadAccountByToken calls Decrypter with correct value

--- src/data/account/load-account-by-token/db-load-account-by-token.ts
+++ src/data/account/load-account-by-token/db-load-account-by-token.ts
@@ -1,9 +1,17 @@
+import { AccountModel } from '../../../domain/models/account';
+import { LoadAccountByToken } from '../../../domain/use-cases/account/load-account-by-token';
 import { LoadAccountByIdRepository } from '../../protocols/account/load-account-by-id-repository';
-import { Decrypter } from '../../protocols/cryptography/descrypter';
+import { Decrypter } from '../../protocols/cryptography/decrypter';
 
-export class DbLoadAccountByToken {
+export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor(
     private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
     private readonly decrypter: Decrypter
   ) {}
+
+  async loadByToken(token: string): Promise<AccountModel | null> {
+    const payload = await this.decrypter.decrypt(token);
+
+    return null;
+  }
 }
