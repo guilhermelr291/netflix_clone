72dfaa518648e869f7a060dc26a959d3529f1dae
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-05 18:12:08 -0300

    feat: create LoadAccountByIdRepository

--- src/data/protocols/account/add-account-repository.ts
+++ src/data/protocols/account/add-account-repository.ts
@@ -1,4 +1,4 @@
-import { AddAccount } from '../../domain/use-cases/account/add-account';
+import { AddAccount } from '../../../domain/use-cases/account/add-account';
 
 export interface AddAccountRepository {
   add(data: AddAccount.Params): Promise<void>;
