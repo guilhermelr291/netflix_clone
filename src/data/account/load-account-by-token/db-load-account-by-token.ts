52487b6c1cddfed39a7fbd86a9c244a2ca4c9e77
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-05 18:15:34 -0300

    feat: create Decrypter interface

--- src/data/account/load-account-by-token/db-load-account-by-token.ts
+++ src/data/account/load-account-by-token/db-load-account-by-token.ts
@@ -1,7 +1,9 @@
 import { LoadAccountByIdRepository } from '../../protocols/account/load-account-by-id-repository';
+import { Decrypter } from '../../protocols/cryptography/descrypter';
 
 export class DbLoadAccountByToken {
   constructor(
-    private readonly loadAccountByIdRepository: LoadAccountByIdRepository
+    private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
+    private readonly decrypter: Decrypter
   ) {}
 }
