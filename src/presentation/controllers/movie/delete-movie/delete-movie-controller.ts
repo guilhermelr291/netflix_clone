faca2e33cfd2c652a8c7b179ae84e785e1445c89
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-10 11:24:19 -0300

    feat: ensure DeleteMovieController calls deleteMovie with correct id

---
+++ src/presentation/controllers/movie/delete-movie/delete-movie-controller.ts
@@ -0,0 +1,27 @@
+import { DeleteMovie } from '../../../../domain/use-cases/movie/delete-movie';
+import { ok } from '../../../helpers/http-helper';
+import { Controller } from '../../../protocols/controller';
+import { HttpResponse } from '../../../protocols/http';
+
+export class DeleteMovieController implements Controller {
+  constructor(private readonly deleteMovie: DeleteMovie) {}
+
+  async handle(request: DeleteMovieController.Params): Promise<HttpResponse> {
+    try {
+      const { id } = request;
+
+      await this.deleteMovie.delete(id);
+
+      return ok({ message: 'movie deleted successfully' });
+    } catch (error) {
+      console.error('Error when deleting movie: ', error);
+      throw error;
+    }
+  }
+}
+
+export namespace DeleteMovieController {
+  export type Params = {
+    id: number;
+  };
+}
