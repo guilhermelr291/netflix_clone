373dbc93e51e7ddcdb09ce51739588ae0286d1e5
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-10 11:07:40 -0300

    feat: ensure MovieRepository calls prisma with correct value

--- src/infra/db/movie/movie-repository.ts
+++ src/infra/db/movie/movie-repository.ts
@@ -1,11 +1,15 @@
 import prisma from '../../../../prisma/db';
 import { AddMovieRepository } from '../../../data/protocols/movie/add-movie-repository';
+import { DeleteMovieRepository } from '../../../data/protocols/movie/delete-movie-repository';
 import { LoadMovieByTitleRepository } from '../../../data/protocols/movie/load-movie-by-title-repository';
 import { Movie } from '../../../domain/models/movie';
 import { AddMovie } from '../../../domain/use-cases/movie/add-movie';
 
 export class MovieRepository
-  implements LoadMovieByTitleRepository, AddMovieRepository
+  implements
+    LoadMovieByTitleRepository,
+    AddMovieRepository,
+    DeleteMovieRepository
 {
   async loadByTitle(title: string): Promise<Movie | null> {
     const movie = await prisma.movie.findUnique({ where: { title } });
@@ -16,5 +20,9 @@ export class MovieRepository
     const movie = await prisma.movie.create({ data });
 
     return movie;
+  }
+
+  async deleteById(id: number): Promise<void> {
+    await prisma.movie.delete({ where: { id } });
   }
 }
