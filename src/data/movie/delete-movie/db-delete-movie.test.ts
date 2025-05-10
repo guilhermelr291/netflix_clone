e6238781edb6d50eff104ab3f6c3a38778bc2525
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-10 11:02:21 -0300

    test: ensure DbDeleteMovie calls DeleteMovieRepository with correct id

---
+++ src/data/movie/delete-movie/db-delete-movie.test.ts
@@ -0,0 +1,35 @@
+import { vi, test, describe, expect } from 'vitest';
+import { DbDeleteMovie } from './db-delete-movie';
+import { DeleteMovieRepository } from '../../protocols/movie/delete-movie-repository';
+import { DeleteMovie } from '../../../domain/use-cases/movie/delete-movie';
+
+const makeDeleteMovieRepository = () => {
+  class DeleteMovieRepositoryStub implements DeleteMovieRepository {
+    async deleteById(id: number): Promise<void> {}
+  }
+  return new DeleteMovieRepositoryStub();
+};
+
+type SutTypes = {
+  sut: DbDeleteMovie;
+  deleteMovieRepositoryStub: DeleteMovieRepository;
+};
+
+const makeSut = (): SutTypes => {
+  const deleteMovieRepositoryStub = makeDeleteMovieRepository();
+  const sut = new DbDeleteMovie(deleteMovieRepositoryStub);
+  return {
+    sut,
+    deleteMovieRepositoryStub,
+  };
+};
+
+describe('DbDeleteMovie UseCase', () => {
+  test('Should call DeleteMovieRepository with correct id', async () => {
+    const { sut, deleteMovieRepositoryStub } = makeSut();
+    const deleteByIdSpy = vi.spyOn(deleteMovieRepositoryStub, 'deleteById');
+    const id = 1;
+    await sut.delete(id);
+    expect(deleteByIdSpy).toHaveBeenCalledWith(id);
+  });
+});
