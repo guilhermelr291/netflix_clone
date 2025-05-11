dbbfd9dc4040434368aaa739ddd7aeda14a9fdbc
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-11 19:45:17 -0300

    feat: implement loadById use-case in MovieRepository

--- src/infra/db/movie/movie-repository.ts
+++ src/infra/db/movie/movie-repository.ts
@@ -1,6 +1,7 @@
 import prisma from '../../../../prisma/db';
 import { AddMovieRepository } from '../../../data/protocols/movie/add-movie-repository';
 import { DeleteMovieRepository } from '../../../data/protocols/movie/delete-movie-repository';
+import { LoadMovieByIdRepository } from '../../../data/protocols/movie/load-movie-by-id-repository';
 import { LoadMovieByTitleRepository } from '../../../data/protocols/movie/load-movie-by-title-repository';
 import { Movie } from '../../../domain/models/movie';
 import { AddMovie } from '../../../domain/use-cases/movie/add-movie';
@@ -9,7 +10,8 @@ export class MovieRepository
   implements
     LoadMovieByTitleRepository,
     AddMovieRepository,
-    DeleteMovieRepository
+    DeleteMovieRepository,
+    LoadMovieByIdRepository
 {
   async loadByTitle(title: string): Promise<Movie | null> {
     const movie = await prisma.movie.findUnique({ where: { title } });
@@ -24,5 +26,9 @@ export class MovieRepository
 
   async deleteById(id: number): Promise<void> {
     await prisma.movie.delete({ where: { id } });
+  }
+
+  async loadById(id: number): Promise<Movie | null> {
+    return await prisma.movie.findUnique({ where: { id } });
   }
 }
