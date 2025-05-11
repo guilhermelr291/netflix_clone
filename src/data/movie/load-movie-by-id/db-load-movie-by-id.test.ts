c1fa5fb511576c9bc594cc352fb626a7aea5d5f4
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-11 20:52:39 -0300

    test: ensure DbLoadMovieById throws NotFoundError if  LoadMovieByIdRepository returns null

--- src/data/movie/load-movie-by-id/db-load-movie-by-id.test.ts
+++ src/data/movie/load-movie-by-id/db-load-movie-by-id.test.ts
@@ -2,6 +2,7 @@ import { vi, describe, expect, test } from 'vitest';
 import { LoadMovieByIdRepository } from '../../protocols/movie/load-movie-by-id-repository';
 import { Movie } from '../../../../generated/prisma';
 import { DbLoadMovieById } from './db-load-movie-by-id';
+import { NotFoundError } from '../../../shared/errors';
 
 const mockMovie = (): Movie => ({
   id: 1,
@@ -50,5 +51,15 @@ describe('DbLoadMovieById', () => {
     const id = 1;
     const movie = await sut.loadById(id);
     expect(movie).toEqual(mockMovie());
+  });
+  test('should throw NotFoundError if LoadMovieByIdRepository returns null', async () => {
+    const { sut, loadMovieByIdRepositoryStub } = makeSut();
+    vi.spyOn(loadMovieByIdRepositoryStub, 'loadById').mockResolvedValueOnce(
+      null
+    );
+
+    await expect(sut.loadById(1)).rejects.toThrow(
+      new NotFoundError('Movie not found')
+    );
   });
 });
