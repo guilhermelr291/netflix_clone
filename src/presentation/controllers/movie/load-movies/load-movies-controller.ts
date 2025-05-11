1c5bd15120fb65427c7904bb9a3690bf7644ae8b
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-11 20:23:15 -0300

    feat: create LoadMoviesController

---
+++ src/presentation/controllers/movie/load-movies/load-movies-controller.ts
@@ -0,0 +1,19 @@
+import { loadMovies } from '../../../../domain/use-cases/movie/load-movies';
+import { ok } from '../../../helpers/http-helper';
+import { Controller } from '../../../protocols/controller';
+import { HttpResponse } from '../../../protocols/http';
+
+export class LoadMoviesController implements Controller {
+  constructor(private readonly loadMovies: loadMovies) {}
+
+  async handle(request: any): Promise<HttpResponse> {
+    try {
+      const movies = await this.loadMovies.loadAll();
+
+      return ok(movies);
+    } catch (error) {
+      console.error('Error on loading movies', error);
+      throw error;
+    }
+  }
+}
