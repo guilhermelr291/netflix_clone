7556b7cb77641ef6362b516c9524808a0c4519f2
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-15 19:47:04 -0300

    feat: implement LoadEpisodeByIdRepository protocol

--- src/infra/db/episode/episode-repository.ts
+++ src/infra/db/episode/episode-repository.ts
@@ -1,12 +1,16 @@
 import prisma from '../../../../prisma/db';
 import { AddEpisodeRepository } from '../../../data/protocols/episode/add-episode-repository';
 import { DeleteEpisodeRepository } from '../../../data/protocols/episode/delete-episode-repository';
+import { LoadEpisodeByIdRepository } from '../../../data/protocols/episode/load-episode-by-id-repository';
 import { Episode } from '../../../domain/models/episode';
 import { AddEpisode } from '../../../domain/use-cases/episode/add-episode';
 import { EpisodeMapper } from '../protocols/episode-mapper';
 
 export class EpisodeRepository
-  implements AddEpisodeRepository, DeleteEpisodeRepository
+  implements
+    AddEpisodeRepository,
+    DeleteEpisodeRepository,
+    LoadEpisodeByIdRepository
 {
   constructor(private readonly episodeMapper: EpisodeMapper) {}
   async add(data: AddEpisode.Params): Promise<Episode> {
@@ -25,5 +29,19 @@ export class EpisodeRepository
         id,
       },
     });
+  }
+
+  async loadById(id: number): Promise<Episode | null> {
+    const episode = await prisma.episode.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!episode) {
+      return null;
+    }
+
+    return this.episodeMapper.toDomain(episode);
   }
 }
