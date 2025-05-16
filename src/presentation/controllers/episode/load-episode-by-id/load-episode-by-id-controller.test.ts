81ddbbee0b69af1a900da33dfd61c4b2f549d8a7
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-16 17:57:13 -0300

    test: ensure LoadEpisodeByIdController returns 200 and Episode on success

--- src/presentation/controllers/episode/load-episode-by-id/load-episode-by-id-controller.test.ts
+++ src/presentation/controllers/episode/load-episode-by-id/load-episode-by-id-controller.test.ts
@@ -1,7 +1,10 @@
-import { vi, test, expect, describe } from 'vitest';
+import { vi, test, expect, describe, afterAll, beforeAll } from 'vitest';
 import { makeLoadEpisodeByIdStub } from '../../../../__tests__/factories/episode/domain-factory';
 import { LoadEpisodeByIdController } from './load-episode-by-id-controller';
 import { LoadEpisodeById } from '../../../../domain/use-cases/episode/load-episode-by-id';
+import { ok } from '../../../helpers/http-helper';
+import { mockEpisode } from '../../../../__tests__/factories/episode/models-factory';
+import mockDate from 'mockdate';
 
 type SutTypes = {
   sut: LoadEpisodeByIdController;
@@ -22,10 +25,22 @@ const mockRequest = {
 };
 
 describe('LoadEpisodeByIdController', () => {
+  beforeAll(() => {
+    mockDate.set(new Date());
+  });
+  afterAll(() => {
+    mockDate.reset();
+  });
+
   test('should call LoadEpisodeById with correct values', async () => {
     const { sut, loadEpisodeByIdStub } = makeSut();
     const loadSpy = vi.spyOn(loadEpisodeByIdStub, 'load');
     await sut.handle(mockRequest);
     expect(loadSpy).toHaveBeenCalledWith(mockRequest.id);
+  });
+  test('should return 200 and Episode on success', async () => {
+    const { sut } = makeSut();
+    const response = await sut.handle(mockRequest);
+    expect(response).toEqual(ok(mockEpisode()));
   });
 });
