afbb666b8377c7ebbaedf493d25ecd0744afef29
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-16 17:42:25 -0300

    test: ensure DbLoadEpisodeById calls LoadEpisodeByIdRepository with correct value

---
+++ src/data/episode/load-episode-by-id/db-load-episode-by-id.test.ts
@@ -0,0 +1,28 @@
+import { vi, test, describe, expect } from 'vitest';
+import { DbLoadEpisodeById } from './db-load-episode-by-id';
+import { LoadEpisodeByIdRepository } from '../../protocols/episode/load-episode-by-id-repository';
+import { makeLoadEpisodeByIdRepository as makeLoadEpisodeByIdRepositoryStub } from '../../../__tests__/factories/episode/infra-factory';
+
+type SutTypes = {
+  sut: DbLoadEpisodeById;
+  loadEpisodeByIdRepositoryStub: LoadEpisodeByIdRepository;
+};
+
+const makeSut = (): SutTypes => {
+  const loadEpisodeByIdRepositoryStub = makeLoadEpisodeByIdRepositoryStub();
+  const sut = new DbLoadEpisodeById(loadEpisodeByIdRepositoryStub);
+  return {
+    sut,
+    loadEpisodeByIdRepositoryStub,
+  };
+};
+
+describe('DbLoadEpisodeById', () => {
+  test('should call LoadEpisodeByIdRepository with correct value', async () => {
+    const { sut, loadEpisodeByIdRepositoryStub } = makeSut();
+    const loadByIdSpy = vi.spyOn(loadEpisodeByIdRepositoryStub, 'loadById');
+    const id = 'any_id';
+    await sut.load(id);
+    expect(loadByIdSpy).toHaveBeenCalledWith(id);
+  });
+});
