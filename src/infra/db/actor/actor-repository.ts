2ff90adc19aafc374b279657bf73b147153767cc
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-17 21:29:12 -0300

    feat: implement DeleteActorRepository in ActorRepository

--- src/infra/db/actor/actor-repository.ts
+++ src/infra/db/actor/actor-repository.ts
@@ -1,10 +1,13 @@
 import prisma from '../../../../prisma/db';
 import { AddActorRepository } from '../../../data/protocols/actor/add-actor-repository';
+import { DeleteActorRepository } from '../../../data/protocols/actor/delete-actor-repository';
 import { Actor } from '../../../domain/models/actor';
 import { AddActor } from '../../../domain/use-cases/actor/add-actor';
 import { ActorMapper } from '../protocols/actor-mapper';
 
-export class ActorRepository implements AddActorRepository {
+export class ActorRepository
+  implements AddActorRepository, DeleteActorRepository
+{
   constructor(private readonly actorMapper: ActorMapper) {}
   async add(data: AddActor.Params): Promise<Actor> {
     const actor = await prisma.actor.create({
@@ -12,5 +15,12 @@ export class ActorRepository implements AddActorRepository {
     });
 
     return this.actorMapper.toDomainModel(actor);
+  }
+  async delete(id: string): Promise<void> {
+    await prisma.actor.delete({
+      where: {
+        id: Number(id),
+      },
+    });
   }
 }
