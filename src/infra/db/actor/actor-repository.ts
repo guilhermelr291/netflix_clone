bfcddd65ce4e60037f67e39f09cc668800da3f91
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-17 21:29:56 -0300

    feat: implement LoadActorByIdRepository in ActorRepository

--- src/infra/db/actor/actor-repository.ts
+++ src/infra/db/actor/actor-repository.ts
@@ -1,12 +1,13 @@
 import prisma from '../../../../prisma/db';
 import { AddActorRepository } from '../../../data/protocols/actor/add-actor-repository';
 import { DeleteActorRepository } from '../../../data/protocols/actor/delete-actor-repository';
+import { LoadActorByIdRepository } from '../../../data/protocols/actor/load-actor-by-id-repository';
 import { Actor } from '../../../domain/models/actor';
 import { AddActor } from '../../../domain/use-cases/actor/add-actor';
 import { ActorMapper } from '../protocols/actor-mapper';
 
 export class ActorRepository
-  implements AddActorRepository, DeleteActorRepository
+  implements AddActorRepository, DeleteActorRepository, LoadActorByIdRepository
 {
   constructor(private readonly actorMapper: ActorMapper) {}
   async add(data: AddActor.Params): Promise<Actor> {
@@ -22,5 +23,18 @@ export class ActorRepository
         id: Number(id),
       },
     });
+  }
+  async loadById(id: string): Promise<Actor | null> {
+    const actor = await prisma.actor.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!actor) {
+      return null;
+    }
+
+    return this.actorMapper.toDomainModel(actor);
   }
 }
