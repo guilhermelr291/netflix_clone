2e2e056589d177b4afcc42ff2e0464154a8c8d33
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-19 22:37:56 -0300

    feat: create UpdateActor use case

---
+++ src/domain/use-cases/actor/update-actor.ts
@@ -0,0 +1,9 @@
+import { Actor } from '../../models/actor';
+
+export interface UpdateActor {
+  update: (actorId: string, actorData: UpdateActor.Params) => Promise<Actor>;
+}
+
+export namespace UpdateActor {
+  export type Params = Partial<Omit<Actor, 'id'>>;
+}
