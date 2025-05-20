9d3367f299d080121c56ea5ea2c34a755cb992cc
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-19 22:41:34 -0300

    feat: implement update method in DbUpdateActor

--- src/data/actor/update-actor/db-update-actor.ts
+++ src/data/actor/update-actor/db-update-actor.ts
@@ -1,11 +1,24 @@
 import { Actor } from '../../../domain/models/actor';
 import { UpdateActor } from '../../../domain/use-cases/actor/update-actor';
+import { NotFoundError } from '../../../shared/errors';
+import { LoadActorByIdRepository } from '../../protocols/actor/load-actor-by-id-repository';
 import { UpdateActorRepository } from '../../protocols/actor/update-actor-repository';
 
 export class DbUpdateActor implements UpdateActor {
-  constructor(private readonly updateActorRepository: UpdateActorRepository) {}
-  async update(
-    actorId: string,
-    actorData: UpdateActor.Params
-  ): Promise<Actor> {}
+  constructor(
+    private readonly updateActorRepository: UpdateActorRepository,
+    private readonly loadActorByIdRepository: LoadActorByIdRepository
+  ) {}
+  async update(actorId: string, actorData: UpdateActor.Params): Promise<Actor> {
+    const actor = await this.loadActorByIdRepository.loadById(actorId);
+    if (!actor) {
+      throw new NotFoundError('Actor not found');
+    }
+
+    const updatedActor = await this.updateActorRepository.update(
+      actorId,
+      actorData
+    );
+    return updatedActor;
+  }
 }
