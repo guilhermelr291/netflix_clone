384b8ac7a9ff4624fa7a3687a91f9da6da5481e9
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-25 12:41:45 -0300

    test: ensure DbLoadActorById returns an actor on success

--- src/data/actor/load-actor-by-id/db-load-actor-by-id.test.ts
+++ src/data/actor/load-actor-by-id/db-load-actor-by-id.test.ts
@@ -2,6 +2,7 @@ import { vi, test, describe, expect } from 'vitest';
 import { makeLoadActorByIdRepository } from '../../../__tests__/factories/actor/infra-factory';
 import { DbLoadActorById } from './db-load-actor-by-id';
 import { LoadActorByIdRepository } from '../../protocols/actor/load-actor-by-id-repository';
+import { mockActor } from '../../../__tests__/factories/actor/models-factory';
 
 type SutTypes = {
   sut: DbLoadActorById;
@@ -25,5 +26,12 @@ describe('DbLoadActorById', () => {
     await sut.loadById('any_id');
 
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id');
+  });
+  test('should return an actor on success', async () => {
+    const { sut } = makeSut();
+
+    const actor = await sut.loadById('any_id');
+
+    expect(actor).toEqual(mockActor());
   });
 });
