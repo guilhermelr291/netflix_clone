eaae82d1848cf0535552b97fb44e7ae3f1e237ae
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-05-25 12:40:35 -0300

    test: ensure DbLoadActorById calls LoadActorByIdRepository with correct values

---
+++ src/data/actor/load-actor-by-id/db-load-actor-by-id.test.ts
@@ -0,0 +1,29 @@
+import { vi, test, describe, expect } from 'vitest';
+import { makeLoadActorByIdRepository } from '../../../__tests__/factories/actor/infra-factory';
+import { DbLoadActorById } from './db-load-actor-by-id';
+import { LoadActorByIdRepository } from '../../protocols/actor/load-actor-by-id-repository';
+
+type SutTypes = {
+  sut: DbLoadActorById;
+  loadActorByIdRepositoryStub: LoadActorByIdRepository;
+};
+
+const makeSut = () => {
+  const loadActorByIdRepositoryStub = makeLoadActorByIdRepository();
+  const sut = new DbLoadActorById(loadActorByIdRepositoryStub);
+  return {
+    sut,
+    loadActorByIdRepositoryStub,
+  };
+};
+
+describe('DbLoadActorById', () => {
+  test('should call LoadActorByIdRepository with correct values', async () => {
+    const { sut, loadActorByIdRepositoryStub } = makeSut();
+    const loadByIdSpy = vi.spyOn(loadActorByIdRepositoryStub, 'loadById');
+
+    await sut.loadById('any_id');
+
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id');
+  });
+});
